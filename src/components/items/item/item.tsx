0bf8f81adabe2804d6d1a5584eb03775f251ab65
Emil Babra <brittsboemil@gmail.com>
2020-10-01 15:25:56 +0200

    Products now fetches proper images

--- src/components/items/item/item.tsx
+++ src/components/items/item/item.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './item.module.css';
-import ItemPic from '../../../images/Dominus_body_icon.png';
+// import images from '../../../images/products';
 
 interface itemProps {
   item: {
@@ -17,7 +17,7 @@ const Item = (props: itemProps) => {
   return (
     <div className={`${styles.container} bg-primary shadowed`}>
       <h4>{name}</h4>
-      <img src={ItemPic} alt="" />
+      <img src={require(`../../../images/products/${img}`)} alt="" />
       <p>Category: {type}</p>
       <p>Price: {price} EUR</p>
       <button className={`btn`}>Add To Cart</button>
